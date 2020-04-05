1bb383182ba77d3d7627a1169cfd6802d005c1b8
RiskBala <44400207+RiskBala@users.noreply.github.com>
2020-04-05 15:20:01 +0530

    Update Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -48,7 +48,7 @@ export default class Home extends Component {
     handleClick = (eventId)=>{
      axios({
       method: 'post',
-      url: 'http://zuulserver-s2-travelsystem.apps.na311.openshift.opentlc.com/registration/v1/'+eventId,
+      url: 'http://eventdetail-s2-travelsystem.apps.na311.openshift.opentlc.com/registration/v1/'+eventId,
       headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + this.state.authtoken,
